f279c299c8c8c1e2fbf0a80da9f37daa9894c0db
arianigarciam <arianigarciam@gmail.com>
2021-03-05 15:56:18 -0400

    01-tipos-basicos seccion 3

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,8 +1,12 @@
-
-
-console.log('Hola Mundo!');
-
 /*
     ===== Código de TypeScript =====
 */
 
+let nombre: string= 'strider';
+let hp: number | string = 123;
+let estaVivo: boolean = true;
+
+nombre = 'jjjjj';
+hp = 'FULL';
+
+console.log(nombre, hp);
