92b92152287dd749f412e64ea2e80daf24d4bd5b
arianigarciam <arianigarciam@gmail.com>
2021-03-06 15:18:51 -0400

    19 Clase

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -27,7 +27,15 @@ const reproductor:Reproductor ={
 const autor = "aaa";
 const { volumen, segundo, cancion, detalles: {autor: autorDetalle} } = reproductor;
 //const { autor } = detalles 
-console.log("El volumen actual es de: ",volumen);
-console.log("El segundo actual es de: ",segundo);
-console.log("La cancion actual es de: ",cancion);
-console.log("El autor es: ",autorDetalle);
+// console.log("El volumen actual es de: ",volumen);
+// console.log("El segundo actual es de: ",segundo);
+// console.log("La cancion actual es de: ",cancion);
+// console.log("El autor es: ",autorDetalle);
+
+const dbz: string[] = ['A','B','C'];
+const [ , ,p3] =dbz;
+
+// console.log('Personaje 1:',p1);
+// console.log('Personaje 2:',p1);
+console.log('Personaje 3:',p3);
+
