360422fa043454e39709d39dbfdacc679756649d
arianigarciam <arianigarciam@gmail.com>
2021-03-06 15:09:59 -0400

    Clase 18

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -2,33 +2,32 @@
     ===== Código de TypeScript =====
 */
 
-interface SuperHeroe {
-    nombre: string;
-    edad : number;
-    direccion: Direccion;
-    mostrarDireccion: () => string;
-   
-}
-    
-interface Direccion {
-    calle: string,
-    pais: string,
-    ciudad: string,
-};
 
-const superHeroe: SuperHeroe = {
-    nombre: 'Spiderman',
-    edad: 30,
-    direccion: {
-        calle: 'Main st',
-        pais: 'USA',
-        ciudad: 'NY'
-    },
-    mostrarDireccion() {
-        return this.nombre + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais;
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalle;
+}
+
+interface Detalle {
+    autor: string;
+    year: number;
+}
+
+const reproductor:Reproductor ={
+    volumen: 90,
+    segundo: 36,
+    cancion: "Mi cancion",
+    detalles: {
+        autor: "Mi autor",
+        year: 1996
     }
 }
-
-const direccion = superHeroe.mostrarDireccion();
-console.log(direccion);
-
+const autor = "aaa";
+const { volumen, segundo, cancion, detalles: {autor: autorDetalle} } = reproductor;
+//const { autor } = detalles 
+console.log("El volumen actual es de: ",volumen);
+console.log("El segundo actual es de: ",segundo);
+console.log("La cancion actual es de: ",cancion);
+console.log("El autor es: ",autorDetalle);
